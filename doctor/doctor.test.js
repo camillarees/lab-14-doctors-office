6025a20fce18f41862504694d80239d4588cc1cc
Mandela Steele-Dadzie <msteeledadzie@gmail.com>
2022-12-19 01:19:04 -0700

    added preliminary tests

--- doctor/doctor.test.js
+++ doctor/doctor.test.js
@@ -1,19 +1,33 @@
 'use strict';
 
-const { io } = require('socket.io-client');
-const socket = io('http://localhost:3002/doctorsoffice');
+let socket = require('../hub');
 
-jest.mock('../hub.js', () => {
+jest.mock('../hub', () => {
   return {
     on: jest.fn(),
     emit: jest.fn(),
   };
 });
 console.log = jest.fn();
-
-describe('Handle PATIENT IS READY', () => {
-  test('emit PATIENT IS READY to receive console log', () => {
+describe('Doctor', () => {
+  it('Emits appointment as expected', () => {
+    const payload = {
+        doctor: 'Doctor Flores',
+        patientId: 'test1234',
+        name: 'Camilla',
+        appointment: '12.18.22',
+    };
     socket.emit('PATIENT IS READY', payload);
     expect(console.log).toHaveBeenCalledWith(`Patient number ${payload.patientId} is ready to be seen by Doctor Flores`);
-  });
-});
+  })
+  it('Confirms completed appointment as expected', () => {
+    const payload = {
+        doctor: 'Doctor Flores',
+        patientId: 'test1234',
+        name: 'Camilla',
+        appointment: '12.18.22'
+    };
+    socket.emit('APPOINTMENT COMPLETE', payload);
+    expect(console.log).toHaveBeenCalledWith(`You saw patient number ${payload.patientId}`);
+  })
+})
